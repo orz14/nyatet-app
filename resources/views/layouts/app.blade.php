d739b8e5d61e977cdedc4eea56fdfd1aa7953f7c
orz14 <oriezt.id@gmail.com>
2023-05-15 19:39:53 +0700

    commit 11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,21 +12,6 @@
 
         <x-footer />
 
-        <script>
-            feather.replace()
-        </script>
-
-        @isset($ckeditor)
-        <script src="https://cdn.ckeditor.com/ckeditor5/36.0.0/classic/ckeditor.js"></script>
-        <script>
-            ClassicEditor
-            .create(document.querySelector('#editor'),{
-                toolbar: ['bold', 'italic', 'link', 'bulletedList', 'undo', 'redo'],
-            })
-            .catch(error => {
-                console.error( error );
-            });
-        </script>
-        @endisset
+        @include('partials.script')
     </body>
 </html>
