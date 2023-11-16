34c4cef19e43d5e4fdbf8c402e938c1dd2f50828
Oriz Wahyu N <oriezt.id@gmail.com>
2023-11-17 00:47:26 +0700

    refactor some code

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -12,17 +12,12 @@ class TodoList extends Component
 {
     public $datas = [];
 
+    #[On('todoAdded')]
     public function render()
     {
         $this->datas = Todo::whereUserId(auth()->user()->id)->whereDate('created_at', Carbon::today())->get();
 
         return view('livewire.todo-list');
-    }
-
-    #[On('todoAdded')]
-    public function todoAdded()
-    {
-        $this->datas = Todo::whereUserId(auth()->user()->id)->whereDate('created_at', Carbon::today())->get();
     }
 
     public function update($slug)
