5e9fe7703124dd8a2cfbf791ef27c69cf7161f7a
Oriz Wahyu N <oriezt.id@gmail.com>
2024-02-19 11:43:45 +0700

    core: add html minify

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use DipeshSukhia\LaravelHtmlMinify\Middleware\LaravelMinifyHtml;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -36,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            LaravelMinifyHtml::class,
         ],
 
         'api' => [
