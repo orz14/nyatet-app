f5baad1299ece846f53c95e153458ebfbafe0eb0
Oriz Wahyu N. <oriezt.id@gmail.com>
2025-05-08 20:35:30 +0700

    update: csrf_token using fingerprint and disposable + composer.json

--- app/Http/Controllers/Api/CheckConnectionController.php
+++ app/Http/Controllers/Api/CheckConnectionController.php
@@ -16,25 +16,16 @@ class CheckConnectionController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $get_ip = $request->header('User-Ip') ?? $request->ip();
-        $cache_name = 'csrf_' . str_replace(['.', '='], '', $get_ip);
+        $get_fingerprint = $request->header('Fingerprint_');
+        $cache_name = "csrf_$get_fingerprint";
         $cachedData = Cache::get($cache_name);
 
         if ($cachedData) {
-            if ($cachedData['usage'] < 5) {
-                $csrf_token = $cachedData['csrf_token'];
-            } elseif ($cachedData['usage'] >= 5) {
-                $generate_token = Generate::randomString(32);
-                $cachedData['csrf_token'] = $generate_token;
-                $cachedData['usage'] = 0;
-                Cache::put($cache_name, $cachedData, Carbon::now()->addDays(1));
-                $csrf_token = $generate_token;
-            }
+            $csrf_token = $cachedData['csrf_token'];
         } else {
             $generate_token = Generate::randomString(32);
             Cache::put($cache_name, [
-                'csrf_token' => $generate_token,
-                'usage' => 0
+                'csrf_token' => $generate_token
             ], Carbon::now()->addDays(1));
             $csrf_token = $generate_token;
         }
@@ -42,7 +33,9 @@ class CheckConnectionController extends Controller
         try {
             DB::connection()->getPdo();
 
-            return Response::success(null, ['csrf_token' => Crypt::encryptString($csrf_token)]);
+            return Response::success(null, [
+                'csrf_token' => Crypt::encryptString($csrf_token)
+            ]);
         } catch (\Exception $err) {
             Log::error($err->getMessage());
 
