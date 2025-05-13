6027e715d3326c2c855ae108a4f5489bfe12bda9
Oriz Wahyu N. <oriezt.id@gmail.com>
2025-05-13 10:53:39 +0700

    update: log controller

--- app/Http/Controllers/Api/LogController.php
+++ app/Http/Controllers/Api/LogController.php
@@ -6,11 +6,9 @@ use App\Helpers\Response;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
-use ObjectId\ObjectId;
 
 class LogController extends Controller
 {
