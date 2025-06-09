8d5403d8851a480ed2c3a16b00445e8c6b41e76c
Oriz Wahyu N. <oriezt.id@gmail.com>
2025-06-09 19:01:42 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 // Testing
 Route::get('/test', function () {
     return response()->json([
-        'statusss' => true,
+        'status' => true,
         'message' => Inspiring::quote()
     ]);
 });
